6514b60de3a7b2d756a6b287e3019f99d4c7fec5
Dain Lee <dlee@reingold.com>
2020-01-16 14:33:53 -0500

    filters added - need to modify more

--- public/js/main.js
+++ public/js/main.js
@@ -2539,4 +2539,64 @@ var objectFitImages=function(){"use strict";function t(t,e){return"data:image/sv
 
 } )( jQuery );
 
+( function( $ ) {
+	$( document ).ready( function() {
+// Want to show a particular category on page load (not all the images)? Add this code to the top of the javascript file
+
+
+// Hide all the images
+// $('.all').addClass('hide');
+
+// Show only the landscape category. Change to the category you want to show on page load
+// $('.landscape').removeClass('hide');
+
+// Remove active class from all filter buttons
+// $('.filter').removeClass('active');
+
+// Apply active class to the filter button matching the filter you want to be shown on page load
+// $('.filter-landscape').addClass('active');
+
+
+//---------------------------------------------------------
+//---------------------------------------------------------
+
+
+// Show all
+$('.filter-all').on('click', function() {
+  var $this = $(this);
+  $('.filter').removeClass('active');
+  $this.addClass('active');
+  $('.all').removeClass('hide');
+});
+
+// Show landscape
+$('.filter-landscape').on('click', function() {
+  var $this = $(this);
+  $('.filter').removeClass('active');
+  $this.addClass('active');
+  $('.landscape').removeClass('hide');
+  $('.urban, .portrait').addClass('hide');
+});
+
+// Show urban
+$('.filter-urban').on('click', function() {
+  var $this = $(this);
+  $('.filter').removeClass('active');
+  $this.addClass('active');
+  $('.urban').removeClass('hide');
+  $('.portrait, .landscape').addClass('hide');
+});
+
+// Show portrait
+$('.filter-portrait').on('click', function() {
+  var $this = $(this);
+  $('.filter').removeClass('active');
+  $this.addClass('active');
+  $('.portrait').removeClass('hide');
+  $('.landscape, .urban').addClass('hide');
+});
+
+
+	} );
+} )( jQuery );
 //# sourceMappingURL=main.js.map
