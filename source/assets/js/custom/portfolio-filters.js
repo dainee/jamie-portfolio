cad261a62bb620346c2899aeb82a92e78cbef01f
Dain Lee <dlee@reingold.com>
2020-01-16 16:20:09 -0500

    filter needs to bo polished

--- source/assets/js/custom/portfolio-filters.js
+++ source/assets/js/custom/portfolio-filters.js
@@ -1,60 +1,37 @@
 ( function( $ ) {
 	$( document ).ready( function() {
-// Want to show a particular category on page load (not all the images)? Add this code to the top of the javascript file
+		var filter = $( '.portfolio-filters__filter' );
+		var filterAll = $( '.portfolio-filters__filter-all' );
+		var filterOne = $( '.portfolio-filters__filter-one' );
+		var filterTwo = $( '.portfolio-filters__filter-two' );
+		var imageAll = $( '.portfolio-filters__image' );
+		var imageOne = $( '.portfolio-filters__image-one' );
+		var imageTwo = $( '.portfolio-filters__image-two' );
 
+		// Show all
+		filterAll.on( 'click', function() {
+			var $this = $( this );
+			filter.removeClass( 'active' );
+			$this.addClass( 'active' );
+			imageAll.removeClass( 'hide' );
+		} );
 
-// Hide all the images
-// $('.all').addClass('hide');
+		// Show UI/UX portfolio
+		filterOne.on( 'click', function() {
+			var $this = $( this );
+			filter.removeClass( 'active' );
+			$this.addClass( 'active' );
+			imageOne.removeClass( 'hide' );
+			imageTwo.addClass( 'hide' );
+		} );
 
-// Show only the landscape category. Change to the category you want to show on page load
-// $('.landscape').removeClass('hide');
-
-// Remove active class from all filter buttons
-// $('.filter').removeClass('active');
-
-// Apply active class to the filter button matching the filter you want to be shown on page load
-// $('.filter-landscape').addClass('active');
-
-
-//---------------------------------------------------------
-//---------------------------------------------------------
-
-
-// Show all
-$('.filter-all').on('click', function() {
-  var $this = $(this);
-  $('.filter').removeClass('active');
-  $this.addClass('active');
-  $('.all').removeClass('hide');
-});
-
-// Show landscape
-$('.filter-landscape').on('click', function() {
-  var $this = $(this);
-  $('.filter').removeClass('active');
-  $this.addClass('active');
-  $('.landscape').removeClass('hide');
-  $('.urban, .portrait').addClass('hide');
-});
-
-// Show urban
-$('.filter-urban').on('click', function() {
-  var $this = $(this);
-  $('.filter').removeClass('active');
-  $this.addClass('active');
-  $('.urban').removeClass('hide');
-  $('.portrait, .landscape').addClass('hide');
-});
-
-// Show portrait
-$('.filter-portrait').on('click', function() {
-  var $this = $(this);
-  $('.filter').removeClass('active');
-  $this.addClass('active');
-  $('.portrait').removeClass('hide');
-  $('.landscape, .urban').addClass('hide');
-});
-
-
+		// Show others
+		filterTwo.on( 'click', function() {
+			var $this = $( this );
+			filter.removeClass( 'active' );
+			$this.addClass( 'active' );
+			imageTwo.removeClass( 'hide' );
+			imageOne.addClass( 'hide' );
+		} );
 	} );
 } )( jQuery );
