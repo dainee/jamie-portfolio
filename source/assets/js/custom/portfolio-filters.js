9af5d6b9d9d2be33b9212d291393462c0d319fce
Dain Lee <dlee@reingold.com>
2020-01-17 16:03:53 -0500

    portfolio JS edited

--- source/assets/js/custom/portfolio-filters.js
+++ source/assets/js/custom/portfolio-filters.js
@@ -1,37 +1,27 @@
 ( function( $ ) {
 	$( document ).ready( function() {
-		var filter = $( '.portfolio-filters__filter' );
 		var filterAll = $( '.portfolio-filters__filter-all' );
 		var filterOne = $( '.portfolio-filters__filter-one' );
 		var filterTwo = $( '.portfolio-filters__filter-two' );
-		var imageAll = $( '.portfolio-filters__image' );
 		var imageOne = $( '.portfolio-filters__image-one' );
 		var imageTwo = $( '.portfolio-filters__image-two' );
 
 		// Show all
 		filterAll.on( 'click', function() {
-			var $this = $( this );
-			filter.removeClass( 'active' );
-			$this.addClass( 'active' );
-			imageAll.removeClass( 'hide' );
+			imageOne.show( 'slow' );
+			imageTwo.show( 'slow' );
 		} );
 
 		// Show UI/UX portfolio
 		filterOne.on( 'click', function() {
-			var $this = $( this );
-			filter.removeClass( 'active' );
-			$this.addClass( 'active' );
-			imageOne.removeClass( 'hide' );
-			imageTwo.addClass( 'hide' );
+			imageOne.show( 'slow' );
+			imageTwo.hide( 'slow' );
 		} );
 
 		// Show others
 		filterTwo.on( 'click', function() {
-			var $this = $( this );
-			filter.removeClass( 'active' );
-			$this.addClass( 'active' );
-			imageTwo.removeClass( 'hide' );
-			imageOne.addClass( 'hide' );
+			imageOne.hide( 'slow' );
+			imageTwo.show( 'slow' );
 		} );
 	} );
 } )( jQuery );
