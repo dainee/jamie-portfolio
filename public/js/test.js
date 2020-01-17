621f764e4bae7468e2ac18b05c4f40e70e7370a3
Dain Lee <dlee@reingold.com>
2020-01-17 16:23:19 -0500

    .gitignore Fixed

--- public/js/test.js
+++
@@ -1,5 +0,0 @@
-// TODO: Remove this file, it's just an example
-console.log( 'this is vendor.js' );
-// TODO: Remove this file, it's just an example
-console.log( 'this is test2.js' ); // eslint-disable-line no-console
-//# sourceMappingURL=test.js.map
